dea060bb953852c939f62bd5e168fae4de7a3323
Ángel Pérez <angelperez.r007@gmail.com>
2022-09-08 17:05:30 -0400

    About ready

--- main.mjs
+++ main.mjs
@@ -3,12 +3,12 @@
 
 const frases = [
     "Lo que conocemos es una gota, lo que no conocemos es un oceano.",
-    // "El aprendizaje es la ciencia, todo lo demás es información.",
-    // "La ciencia nunca resuelve un problema sin crear otros 10 más (así como el css).",
-    // "Esfuerzate por progresos, no por perfección.",
-    // "Existen oportunidades secretas dentro de cada fracaso.",
-    // "Si el plan no funciona, cambia el plan, pero no la meta.",
-    // ".   .   ."
+    "El aprendizaje es la ciencia, todo lo demás es información.",
+    "La ciencia nunca resuelve un problema sin crear otros 10 más (así como el css).",
+    "Esfuerzate por progresos, no por perfección.",
+    "Existen oportunidades secretas dentro de cada fracaso.",
+    "Si el plan no funciona, cambia el plan, pero no la meta.",
+    ".   .   ."
 ]
 
 const mecano = document.getElementById("mecano")
@@ -21,14 +21,14 @@ function mecanography() {
 
             setTimeout(() => {
                 let arr = mode.split("")
-                let x = ""
+                // let x = ""
                 let html = ""
                 let i = 0
                 let intervaloLetras = setInterval(function(){
-                    x += arr[i]
+                    // x += arr[i]
                     html += arr[i]
                     mecano.innerText = html
-                    console.log(x);
+                    // console.log(x);
                     i++
                     if(i === arr.length){
                         clearInterval(intervaloLetras)
@@ -37,11 +37,8 @@ function mecanography() {
             }, index * interval)
         })
     }
-    // return true
+    intervaloFrase()
     setInterval(intervaloFrase, frases.length * interval)
 }
 
-
-// while(mecanography()){
 mecanography()
-// }
